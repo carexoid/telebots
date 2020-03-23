730911203c1d4670884bdf913140bb62d246d9c7
carexoid <osokadania@gmail.com>
2020-03-23 16:50:28 +0200

    Added clculating expedition

--- avalon_bot/bot.py
+++ avalon_bot/bot.py
@@ -103,7 +103,7 @@ def callback_inline(call):
 
 @bot.message_handler(commands=['vote_for_expedition'])
 def voter(msg):
-    if len(msg.text.split()) < 4:
+    if len(msg.text.split()) < 1:
         bot.reply_to(msg, 'To few expeditors')
     else:
         exp_id = []
@@ -112,6 +112,8 @@ def voter(msg):
         for nick in nicks:
             print(nick)
             exp_id.append(players_id[msg.chat.id].players_nick_to_id[nick])
+        for player in players_id[msg.chat.id].players.keys():
+            players_id[msg.chat.id].cur_voting_for_exp[player] = None
         gameplay.vote_for_exp(exp_id, msg.chat.id)
         players_id[msg.chat.id].cur_exp = exp_id
         players_id[msg.chat.id].state = 'vote'
@@ -133,30 +135,69 @@ def abort(msg):
                                           or 'I don`t like it in chat ' in message.text))
 def get_vote(msg):
     try:
-        print(msg.text)
         chat_id = int(msg.text.split().pop())
-        print(chat_id)
-        if not players_id[chat_id].cur_voting_for_exp[msg.from_user.id]:
-            players_id[chat_id].cur_voting_for_exp[msg.from_user.id] = (1 if msg.text.split()[1] == 'like' else -1)
-            keyboard = telebot.types.ReplyKeyboardMarkup()
-            bot.send_message(msg.chat.id, "You voted for this expedition", reply_markup=None)
-        sum = 0
-        people_votes = ''
-        for vote in players_id[chat_id].cur_voting_for_exp.values():
-            if not vote:
-                return
-            sum += int(vote)
-        for player in players_id[chat_id].cur_voting_for_exp.keys():
-            people_votes += '\n@' + str(bot.get_chat_member(chat_id, player).user.username) \
-                            + (' +1' if players_id[chat_id].cur_voting_for_exp[player] == 1 else ' -1')
-        bot.send_message(chat_id,
-                         ('there will be such expedition' if sum > 0 else 'There won`t be such expedition') + people_votes)
-        if sum > 0:
-            players_id[chat_id].state = 'exp'
-            gameplay.start_exp(players_id[chat_id].cur_exp, chat_id)
+        if players_id[chat_id].state == 'vote':
+            print(msg.text)
+            print(chat_id)
+            if not players_id[chat_id].cur_voting_for_exp[msg.from_user.id]:
+                players_id[chat_id].cur_voting_for_exp[msg.from_user.id] = (1 if msg.text.split()[1] == 'like' else -1)
+                keyboard = telebot.types.ReplyKeyboardMarkup()
+                bot.send_message(msg.chat.id, "You voted for this expedition", reply_markup=None)
+            sum = 0
+            people_votes = ''
+            for vote in players_id[chat_id].cur_voting_for_exp.values():
+                if not vote:
+                    return
+                sum += int(vote)
+            for player in players_id[chat_id].cur_voting_for_exp.keys():
+                people_votes += '\n@' + str(bot.get_chat_member(chat_id, player).user.username) \
+                                + (' +1' if players_id[chat_id].cur_voting_for_exp[player] == 1 else ' -1')
+            bot.send_message(chat_id,
+                             ('there will be such expedition' if sum > 0 else 'There won`t be such expedition') + people_votes)
+            if sum > 0:
+                players_id[chat_id].state = 'exp'
+                players_id[chat_id].people_in_exp = dict()
+                for id in players_id[chat_id].cur_exp:
+                    players_id[chat_id].people_in_exp[id] = None
+                gameplay.start_exp(players_id[chat_id].cur_exp, chat_id)
+            else:
+                players_id[chat_id].state = 'game'
         else:
-            players_id[chat_id].state = 'game'
+            bot.reply_to(msg, 'No voting for expedition right now!')
+
     except KeyError:
         print('bot durila')
 
-bot.polling()
+
+@bot.message_handler(func=lambda message: message.text and ("Peace in chat " in message.text
+                                          or 'War in chat ' in message.text))
+def get_exp_choice(msg):
+    try:
+        chat_id = int(msg.text.split().pop())
+        if players_id[chat_id].people_in_exp[msg.from_user.id] is None:
+            players_id[chat_id].people_in_exp[msg.from_user.id] = 1 if msg.text.split()[0] == 'Peace' else 0
+        sum = 0
+        for choice in players_id[chat_id].people_in_exp.values():
+            if choice is None:
+                return
+            sum += choice
+        print(sum)
+        num_of_exp = players_id[chat_id].successful_exp + players_id[chat_id].failed_exp
+        if gameplay.exp_successful(sum, len(players_id[chat_id].people_in_exp), num_of_exp):
+            bot.send_message(chat_id, 'Expedition was successful')
+            players_id[chat_id].successful_exp += 1
+        else:
+            bot.send_message(chat_id, 'Expedition was failed')
+            players_id[chat_id].failed_exp += 1
+        if players_id[chat_id].failed_exp == 3:
+            bot.send_message(chat_id, 'RIP Avalon!')
+        elif players_id[chat_id].successful_exp == 3:
+            bot.send_message(chat_id, 'Time to shot for Assasin')
+        else:
+            players_id[chat_id].state == 'game'
+
+    except KeyError:
+        print('bot durila x2')
+
+if __name__ == '__main__':
+    bot.polling()
--- avalon_bot/tools.py
+++ avalon_bot/tools.py
@@ -23,7 +23,8 @@ class GameInfo:
         self.cur_lady = cur_lady
         self.successful_exp = 0
         self.failed_exp = 0
-        self.exp_size = []
+        self.exp_size = [0, 0, 0, 0, 0]
         self.players_nick_to_id = dict()
         self.cur_voting_for_exp = dict.copy(players)
         self.cur_exp = []
+        self.people_in_exp = dict()
